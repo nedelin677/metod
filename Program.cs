15745cde725b3f04dcbdbe366c3e6a91f0160e9f
Seslio <svetlozar.temelkov.pp26@pgmtplovdiv.info>
2026-01-13 13:00:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+namespace metod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader();
+            Console.WriteLine("Hello, World!");
+            PrintHeader();
+            GetSqure(5);
+            Console.WriteLine(GetSqure(5));
+
+
+        }
+
+        private static void PrintHeader()
+        {
+            Console.WriteLine("-------------");
+
+
+        }
+
+        private static double GetSqure(double num)
+        {
+            return num * num; 
+        }
+    }
+}
